3ef62c9b11b0203c5b659bc3968dbe061d0f9cb5
chyang <yang.chinghan@gamil.com>
2026-02-06 16:56:27 +0800

    更新範例程式碼資料夾名稱Example-Codes,將assets資料夾移到Example-Codes

--- pytorch2openvino.py
+++ pytorch2openvino.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLO11n-pose PyTorch model
-model = YOLO("sample/yolo11m-pose.pt")
+model = YOLO("yolo11m-pose.pt")
 
 # Export the model
 model.export(format="openvino")  # creates 'yolo11n_openvino_model/'
